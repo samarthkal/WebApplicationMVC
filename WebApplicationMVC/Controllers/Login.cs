cd98b4ba76e950571c57e4f76629750d3e327c0a
samarthkal <kalesamarth08@gmail.com>
2024-03-08 11:25:36 +0530

    Add project files.

---
+++ WebApplicationMVC/Controllers/Login.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationMVC.Controllers
+{
+    public class Login : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplicationMVC/Data/AppDbConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationMVC.Models;
+
+namespace WebApplicationMVC.Data
+{
+    public class AppDbConfig : DbContext
+    {
+
+        public AppDbConfig(DbContextOptions<AppDbConfig> options):base(options) {
+         
+
+        
+        }
+
+       DbSet<Category> Categories { get; set; }
+        
+            
+       
+    }
+}
